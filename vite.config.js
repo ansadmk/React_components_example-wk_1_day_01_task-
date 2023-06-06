50e1f03d887cfe71546da7477caa351d1512838e
Mohemed ansad mk <92428126+ansadmk@users.noreply.github.com>
2023-06-06 11:44:59 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'/React_components_example-wk_1_day_02_task-/'
 })
